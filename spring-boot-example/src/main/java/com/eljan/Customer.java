ce67d4d89b6033295258cb9b59287f4f9ab0989a
Elcan <elcan@Elcans-MacBook-Air-2.local>
2024-02-26 17:55:35 +0400

    connect to database, perform crud

--- spring-boot-example/src/main/java/com/eljan/Customer.java
+++ spring-boot-example/src/main/java/com/eljan/Customer.java
@@ -1,8 +1,23 @@
 package com.eljan;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+
     private Integer ID;
     private String name;
     private String email;
--- spring-boot-example/src/main/java/com/eljan/Main.java
+++ spring-boot-example/src/main/java/com/eljan/Main.java
@@ -2,32 +2,71 @@ package com.eljan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello");
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet(){
-        GreetResponse response = new GreetResponse("hello",
-                List.of("Java", "Js", "Python"),
-                new Person("Eljan", 20, 30_000));
-        return response;
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
 
-    record Person(String name, int age, double savings){}
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-
+    record NewCustomerRequest(String name, String email, Integer age
     ){}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@RequestBody NewCustomerRequest request,@PathVariable("customerId") Integer id){
+        Customer customer = new Customer();
+        customer.setID(id);
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+//    @GetMapping("/greet")
+//    public GreetResponse greet(){
+//        GreetResponse response = new GreetResponse("hello",
+//                List.of("Java", "Js", "Python"),
+//                new Person("Eljan", 20, 30_000));
+//        return response;
+//    }
+//
+//    record Person(String name, int age, double savings){}
+//    record GreetResponse(
+//            String greet,
+//            List<String> favProgrammingLanguages,
+//            Person person
+//
+//    ){}
 }
